64cf4967e5063efb263f5b64de70f6e6584653d7
Bao <Bao@BAOX-MBP-2.attlocal.net>
2023-09-19 20:49:56 -0500

    added pass length and added vars

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,51 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function validateLength(userLength) {
+  return false;
+};
+
+function validateLowercase(userLowercase) {
+  return false;
+}
+
+function generatePassword() {
+  var characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()-=+,.?";
+  var passLength = prompt("Enter password with a length of 8-128 characters.");
+  var Lowercase = prompt("Add a lowercase?");
+  var Uppercase = prompt("Add an uppercase?");
+  var Numeric = prompt("Add numeric?");
+  var SpecialCharacters = prompt("Add special characters?");
+
+
+  console.log(passLength);
+
+  if (validateLength(passLength)) {
+
+  } else {
+    alert("You have entered an invalid password length.")
+    return;
+   };
+
+  // console.log(validateLowercase);
+
+  // if (validateLowercase(addLowercase)) {
+     
+  //} else {
+    // alert("You have added a lowercase.")
+    //return;
+   //};
+  
+};
+
+    //} else if {
+    //alert("You have entered an invalid password character".)
+    
+
+    
+
+
+
+  //return newPass;
