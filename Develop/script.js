173e46e959308a4ab807e50e0611cea7823c3b15
14089 <baoxiong814@gmail.com>
2023-09-24 18:16:51 -0500

    first commit. prompts added and concatenated

--- Develop/script.js
+++ Develop/script.js
@@ -14,49 +14,66 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-function validateLength(userLength) {
-  return false;
-};
+//arrays
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", ",", "<", ".", ">", "?", "/"];
+  var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var randomArray = [];
 
-function validateLowercase(userLowercase) {
-  return false;
-}
+
+//variable declaration
+  var passLength = "";
+  var confirmLowercase;
+  var confirmUppercase;
+  var confirmNumeric;
+  var confirmSpecialCharacters;
+  var password = "";
+
 
 function generatePassword() {
-  var characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()-=+,.?";
-  var passLength = prompt("Enter password with a length of 8-128 characters.");
-  var Lowercase = prompt("Add a lowercase?");
-  var Uppercase = prompt("Add an uppercase?");
-  var Numeric = prompt("Add numeric?");
-  var SpecialCharacters = prompt("Add special characters?");
-
-
-  console.log(passLength);
-
-  if (validateLength(passLength)) {
-
-  } else {
-    alert("You have entered an invalid password length.")
-    return;
-   };
-
-  // console.log(validateLowercase);
-
-  // if (validateLowercase(addLowercase)) {
-     
-  //} else {
-    // alert("You have added a lowercase.")
-    //return;
-   //};
+  var password = "";
+// loop to randomly select from arrays
+  for (var i = 0; i < passLength; i++) {
+      var randomIndex = Math.floor(Math.random() * randomArray.length);
+      password = password + randomArray[randomIndex];
+  } return password;
+}
   
+//if password length is not met, user will be prompted again
+if (passLength < 8 || passLength > 128) {
+    alert("You have entered an invalid password length.");
+    var passLength = prompt("Enter a password length of 8-128 characters.");
+   }
+     
+  //confirms character type(s) to include in password
+  var confirmLowercase = confirm("Include a lowercase?");
+  var confirmUppercase = confirm("Include an uppercase?");
+  var confirmNumeric = confirm("Include numeric?");
+  var confirmSpecialCharacters = confirm("Include special characters?");
+
+//if no character type selected, user is re-prompted to confirm character type(s) 
+if (confirmLowercase === false && confirmUppercase === false && confirmNumeric === false && confirmSpecialCharacters === false) {
+  alert("You must choose one of the following character types to include in the password.");
+    var confirmLowercase = confirm("Include a lowercase?");
+    var confirmUppercase = confirm("Include an uppercase?");
+    var confirmNumeric = confirm("Include numeric?");
+    var confirmSpecialCharacters = confirm("Include special characters?");
 };
 
-    //} else if {
-    //alert("You have entered an invalid password character".)
-    
+     
+//if character type(s) is/are confirmed
+    if (confirmLowercase) {
+      randomArray = randomArray.concat(lowercase);
+    }
+    if (confirmUppercase) {
+      randomArray = randomArray.concat(uppercase);
+    }
+    if (confirmNumeric) {
+      randomArray = randomArray.concat(numeric);
+    }
+    if (confirmSpecialCharacters) {
+      randomArray = randomArray.concat(specialCharacters);
+    };
 
-    
 
-
-
-  //return newPass;
